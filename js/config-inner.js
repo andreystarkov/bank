745146265bf7c298e3efaa24aeb75a05d143c6a5
Andrew <im@andreystarkov.ru>
2014-03-27 08:29:13 +0600

    news-list & some fixes

---
+++ js/config-inner.js
@@ -0,0 +1,68 @@
+
+    window._skel_config = {
+        prefix: 'css/style',
+        preloadStyleSheets: true,
+        resetCSS: false,
+        boxModel: 'border',
+        grid: { gutters: 30 },
+        breakpoints: {
+            wide: { range: '1200-', containers: 1200, grid: { gutters: 50 } },
+            narrow: { range: '481-1100', containers: 960 },
+            narrower: { range: '481-960', containers: 'fluid' },
+            mobile: { range: '-480', containers: 'fluid', lockViewport: true, grid: { collapse: true, gutters: 20 } }
+        }
+    };
+
+    window._skel_panels_config = {
+        panels: {
+            leftPanel: {
+                breakpoints: 'mobile,narrower,narrow',
+                position: 'left',
+                size: 250,
+                html: '<div data-action="moveCell" data-args="left-nav,top-links"></div>'
+            },
+            rightPanel: {
+                breakpoints: 'mobile,narrower,narrow',
+                position: 'right',
+                size: 250,
+                html: '<div data-action="moveCell" data-args="top-nav,top-links"></div>'
+            }
+        },
+        overlays: {
+            titleBar: {
+                breakpoints: 'mobile,narrower,narrow',
+                position: 'top-center',
+                width: '100%',
+                height: 44,
+                html: '<div data-action="copyHTML" data-args="title"></div>'
+            },
+            leftPanelButton: {
+                breakpoints: 'mobile,narrower,narrow',
+                position: 'top-left',
+                width: 80,
+                height: 60,
+                html: '<div class="toggle fa fa-bars fa-2x" data-action="togglePanel" data-args="leftPanel"></div>'
+            },
+            rightPanelButton: {
+                breakpoints: 'mobile,narrower,narrow',
+                position: 'top-right',
+                width: 80,
+                height: 60,
+                html: '<div class="toggle fa fa-home fa-2x" data-action="togglePanel" data-args="rightPanel"></div>'
+            }
+        }
+    };
+
+    $(function () {
+        var $container = $('.responsive-grid');
+        $container.packery({
+          itemSelector: '.item',
+          gutter: 0
+        });
+
+        $('.left-nav a').each(function(){
+            if( $(this).hasClass('active') ){
+                $(this).append('<i class="fa fa-chevron-right"></i>');
+            }
+        });
+    });
--- js/config.js
+++ js/config.js
@@ -41,9 +41,15 @@
 	};
 
 	$(function () {
-	var $container = $('.responsive-grid');
-	$container.packery({
-	  itemSelector: '.item',
-	  gutter: 0
-	});
+		var $container = $('.responsive-grid');
+		$container.packery({
+		  itemSelector: '.item',
+		  gutter: 0
+		});
+
+		$('.left-nav a').each(function(){
+			if( $(this).hasClass('active') ){
+				$(this).append('<i class="fa fa-chevron-right"></i>');
+			}
+		});
 	});
